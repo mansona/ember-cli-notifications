1a2f1f6039370a264724d7b46a86a22825216cd4
Kerry Gallagher <k.gallagher.05@gmail.com>
2014-10-23 09:40:24 +0100

    Make remove work

--- app/components/blooie-notification.js
+++ app/components/blooie-notification.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     actions: {
         removeNotification: function() {
-
+            this.notifications.removeNotification(this.get('notification'));
         }
     }
 });
