5f30fdf5f7a24b6634fefc524a3b1fed5de00417
Kerry Gallagher <k.gallagher.05@gmail.com>
2014-10-23 09:46:41 +0100

    Add className based on type

--- app/components/blooie-notification.js
+++ app/components/blooie-notification.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    classNameBindings: ['notification.type'],
+
     actions: {
         removeNotification: function() {
             this.notifications.removeNotification(this.get('notification'));
