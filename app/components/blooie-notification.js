395e42434cb2a31f4ebc395e254addc8a43b6108
ynnoj <jonathan.steele@me.com>
2014-10-24 16:56:18 +0100

    Positioning, base CSS with alert themes for notifications

--- app/components/blooie-notification.js
+++ app/components/blooie-notification.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    classNameBindings: ['notification.type'],
+    classNameBindings: [':dash-notification', 'notification.type'],
 
     actions: {
         removeNotification: function() {
