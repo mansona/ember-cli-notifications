745ab0cee1edd270043b0386693208c1c02725cf
ynnoj <jonathan.steele@me.com>
2015-01-15 14:51:08 +0000

    Updates to notifications service following changes on new OAuth apps

--- app/components/blooie-notification.js
+++ app/components/blooie-notification.js
@@ -1,7 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    classNameBindings: [':dash-notification', 'notification.type'],
+    classNameBindings: [':notification', 'notification.type', 'notification.dismiss::in', ':pad1', ':center-block'],
+
+    // Set icon depending on notification type
+    notificationIcon: function() {
+        switch(this.get('notification.type')){
+            case "info":
+                return 'fa-info-circle';
+            case "success":
+                return 'fa-check';
+            case "warning":
+            case "error":
+                return 'fa-warning';
+        }
+    }.property('notification.type'),
 
     actions: {
         removeNotification: function() {
