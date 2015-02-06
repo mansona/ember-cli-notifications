7ddc937057fb3b265c6c7a28b23332cd71247ccb
ynnoj <jonathan.steele@me.com>
2015-02-06 11:41:56 +0000

    Adding demo buttons, notifications to the dummy app

---
+++ tests/dummy/app/controllers/application.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+    actions: {
+        showSuccess: function() {
+            this.notifications.addNotification({
+                message: "A success notification",
+                type: 'success'
+            });
+        },
+        showInfo: function() {
+            this.notifications.addNotification({
+                message: "An info notification",
+                type: 'info'
+            });
+        },
+        showError: function() {
+            this.notifications.addNotification({
+                message: "An error notification",
+                type: 'error'
+            });
+        },
+        showWarning: function() {
+            this.notifications.addNotification({
+                message: "A warning notification",
+                type: 'warning'
+            });
+        }
+    }
+});
