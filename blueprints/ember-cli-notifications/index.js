3e006aec4160c95546318605b780715ab34503eb
ynnoj <jonathan.steele@me.com>
2015-03-06 17:42:36 +0000

    Import of Font Awesome is now set by consuming application config, disabled by default. Fixes #8.

--- blueprints/ember-cli-notifications/index.js
+++ blueprints/ember-cli-notifications/index.js
@@ -4,7 +4,17 @@
 module.exports = {
     normalizeEntityName: function() {},
 
-    afterInstall: function() {
-        return this.addBowerPackageToProject('font-awesome');
+    // Use config to determine whether Font Awesome is imported into consuming app
+    afterInstall: function(app) {
+        var projectConfig = this.project.config(app.env);
+        var config = projectConfig['ember-cli-notifications'];
+
+        if (config) {
+            var include = config.includeFontAwesome;
+
+            if (include) {
+                return this.addBowerPackageToProject('font-awesome');
+            }
+        }
     }
 };
--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -16,6 +16,10 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    'ember-cli-notifications': {
+        includeFontAwesome: true
     }
   };
 
