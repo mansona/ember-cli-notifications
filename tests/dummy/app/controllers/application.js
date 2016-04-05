ccdb8f088d287a26ad516fca01bbf3832d48e37b
Jonathan Steele <jonathan@steele.pro>
2016-04-05 11:29:43 +0100

    New docs, demo
    * Starting work on new docs, demo
    
    * Finishing the docs
    
    * Fixing typo

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -2,19 +2,26 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   message: 'Notification message',
+  htmlMessage: '<b>HTML notification message</b>',
   type: 'success',
   autoClear: true,
   clearDuration: 2400,
   htmlContent: false,
   position: 'top',
 
+  computedMessage: Ember.computed('htmlContent', function() {
+    if (this.get('htmlContent')) return this.get('htmlMessage');
+
+    return this.get('message');
+  }),
+
   actions: {
     showNotifcation() {
-      if(this.get('clearAll')) {
+      if (this.get('clearAll')) {
         this.notifications.clearAll();
       }
       this.notifications.addNotification({
-        message: this.get('message'),
+        message: this.get('computedMessage'),
         type: this.get('type'),
         autoClear: this.get('autoClear'),
         clearDuration: this.get('clearDuration'),
