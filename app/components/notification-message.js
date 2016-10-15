5924874e51a7379a0b183072c51834364fe8d833
Jonathan Steele ⚡️ <jonathan@steele.pro>
2016-10-15 16:33:21 +0100

    :sparkles: Allow global values to be defined in app config (#105)
    User configurable properties autoClear and clearDuration can now be set in consuming app config. Fixes #101
    
    :books: Updating dummy app, docs

--- app/components/notification-message.js
+++ app/components/notification-message.js
@@ -1,7 +1,7 @@
 import NotificationMessage from 'ember-cli-notifications/components/notification-message';
 import ENV from '../config/environment';
 
-let config = ENV['ember-cli-notifications'] || {};
+const config = ENV['ember-cli-notifications'] || {};
 
 export default NotificationMessage.extend({
   icons: config.icons || 'font-awesome'
