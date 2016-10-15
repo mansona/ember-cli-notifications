dbe7e94c63869b53390ce7eb1bf27526ac3c3e72
Jonathan Steele ⚡️ <jonathan@steele.pro>
2016-10-15 17:12:14 +0100

    :sparkles: Export notifications service to allow for extension (#109)
    Fixes #108
    
    :books: Updating dummy app, docs

---
+++ tests/dummy/app/services/notifications.js
@@ -0,0 +1,5 @@
+import NotificationsService from 'ember-cli-notifications/services/notification-messages-service';
+
+export default NotificationsService.extend({
+  // Extend the imported service
+});
