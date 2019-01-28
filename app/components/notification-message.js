81b0732b28549f62586489efee7b06bbb62a0da5
Maximillian Meier <mm@reppa.net>
2019-01-28 14:37:49 +0100

    fixed typos

--- app/components/notification-message.js
+++ app/components/notification-message.js
@@ -7,11 +7,11 @@ const globals = config['ember-cli-notifications'] || {}; // Import app config ob
 export default NotificationMessage.extend({
   init() {
     this._super(...arguments);
-    this.icons: globals.icons || 'font-awesome';
-    this.successIcon: globals.successIcon || 'fa fa-check',
-    this.infoIcon: globals.infoIcon || 'fa fa-info-circle',
-    this.warningIcon: globals.warningIcon || 'fa fa-warning',
-    this.errorIcon: globals.errorIcon || 'fa fa-exclamation-circle',
-    this.closeIcon: globals.closeIcon || 'fa fa-times'
+    this.icons = globals.icons || 'font-awesome';
+    this.successIcon = globals.successIcon || 'fa fa-check',
+    this.infoIcon = globals.infoIcon || 'fa fa-info-circle',
+    this.warningIcon = globals.warningIcon || 'fa fa-warning',
+    this.errorIcon = globals.errorIcon || 'fa fa-exclamation-circle',
+    this.closeIcon = globals.closeIcon || 'fa fa-times'
   }
 });
