eac2997474db8e6fb8f52c4ccd139573bfb5499a
Jacob Quant <jacobq@gmail.com>
2019-12-05 10:21:37 -0600

    revert: undo postcss changes
    * Revert "fix: enable postCss filter"
      This reverts commit e16dcc009682dc7227d875e2cf2cc1092482c49f.
    * Revert "fix: add postcss-import"
      This reverts commit 72f238e43fc8c101cbe5ee5a3fb2199af4915236.
    * Revert "fix: add postcssOptions.compile.includePaths"
      This reverts commit 84c953775d87c22b2220215a8488a5ad99afae9e.

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 'use strict';
 
-const CssImport = require('postcss-import');
 const PresetEnv = require('postcss-preset-env');
-const styleDir = require('path').join(__dirname, 'addon', 'styles');
 
-const compileAndFilterOptions = {
-  enabled: true,
-  includePaths: [styleDir],
-  plugins: [
-    { module: CssImport },
-    {
-      module: PresetEnv,
-      options: { stage: 3 },
-      features: {
-        'nesting-rules': true
-      }
-    }
-  ]
-};
 
 module.exports = {
   name: require('./package').name,
 
   options: {
     postcssOptions: {
-      compile: compileAndFilterOptions,
-      filter: compileAndFilterOptions,
+      compile: {
+        enabled: true,
+        plugins: [
+          {
+            module: PresetEnv,
+            options: { stage: 3 },
+            features: {
+              'nesting-rules': true
+            }
+          }
+        ]
+      }
     }
   },
 };
