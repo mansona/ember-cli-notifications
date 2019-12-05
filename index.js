e16dcc009682dc7227d875e2cf2cc1092482c49f
Jacob Quant <jacobq@gmail.com>
2019-12-05 10:17:30 -0600

    fix: enable postCss filter

--- index.js
+++ index.js
@@ -4,25 +4,28 @@ const CssImport = require('postcss-import');
 const PresetEnv = require('postcss-preset-env');
 const styleDir = require('path').join(__dirname, 'addon', 'styles');
 
+const compileAndFilterOptions = {
+  enabled: true,
+  includePaths: [styleDir],
+  plugins: [
+    { module: CssImport },
+    {
+      module: PresetEnv,
+      options: { stage: 3 },
+      features: {
+        'nesting-rules': true
+      }
+    }
+  ]
+};
+
 module.exports = {
   name: require('./package').name,
 
   options: {
     postcssOptions: {
-      compile: {
-        enabled: true,
-        includePaths: [styleDir],
-        plugins: [
-          { module: CssImport },
-          {
-            module: PresetEnv,
-            options: { stage: 3 },
-            features: {
-              'nesting-rules': true
-            }
-          }
-        ]
-      }
+      compile: compileAndFilterOptions,
+      filter: compileAndFilterOptions,
     }
   },
 };
