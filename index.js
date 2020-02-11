302fe580c2c496ae994d204f99248355c561bda3
Chris Manson <chris@manson.ie>
2020-02-11 14:20:04 +0000

    manually running postcss and adding it to vendor

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 'use strict';
 
+const CssImport = require('postcss-import');
 const PresetEnv = require('postcss-preset-env');
+var broccoliPostCSS = require('broccoli-postcss')
+const mergeTrees = require('broccoli-merge-trees');
+var get = require('lodash.get');
 
 
 module.exports = {
   name: require('./package').name,
 
-  options: {
-    postcssOptions: {
-      compile: {
-        enabled: true,
-        plugins: [
-          {
-            module: PresetEnv,
-            options: { stage: 3 },
-            features: {
-              'nesting-rules': true
-            }
-          }
-        ]
-      }
-    }
+  treeForAddon() {
+    var tree = this._super(...arguments);
+
+    // I don't know exactly why targets is private so I am using `get()` to make
+    // sure that it isn't missing
+    let overrideBrowserslist = get(this, 'app.project._targets.browsers');
+
+    let newFile = broccoliPostCSS('prebuilt-styles', {
+      plugins: [
+        CssImport({
+          path: 'prebuilt-styles'
+        }),
+        PresetEnv({
+          stage: 3,
+          features: { 'nesting-rules': true },
+          overrideBrowserslist,
+        })
+      ]});
+
+    return mergeTrees([tree, newFile]);
   },
 };
