3e8ff7ff513882242dfffca6da9f020f5aae7639
Chris Manson <chris@manson.ie>
2020-02-18 22:10:20 +0000

    manually running postcss and adding it to vendor

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,22 +2,9 @@
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
-const PresetEnv = require('postcss-preset-env');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    postcssOptions: {
-      compile: {
-        enabled: true,
-        plugins: [
-          {
-            module: PresetEnv,
-            options: { stage: 3 },
-            features: { 'nesting-rules': true }
-          }
-        ]
-      }
-    },
     minifyCSS: {
       enabled: false
     },
