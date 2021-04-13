9a658efa46a839b333d1f11d792ea9674c8a1ece
Chris Manson <chris@manson.ie>
2021-04-13 11:10:03 +0100

    Drop support for Ember 2.18 and 3.4

--- config/ember-try.js
+++ config/ember-try.js
@@ -8,40 +8,20 @@ module.exports = async function () {
   return {
     scenarios: [
       {
-        name: 'ember-lts-2.18',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1',
-            'ember-source': '~2.18.0'
-          }
-        }
-      },
-      {
-        name: 'ember-lts-3.4',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.4.0'
-          }
-        }
-      },
-      {
-        name: 'ember-lts-3.4',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.4.0'
-          }
-        }
-      },
-      {
         name: 'ember-lts-3.8',
         npm: {
           devDependencies: {
             'ember-source': '~3.8.0'
           }
         }
+      },
+      {
+        name: 'ember-lts-3.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.12.0',
+          },
+        },
       },
       {
         name: 'ember-lts-3.16',
