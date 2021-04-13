22ba57715d9137dd9d3d9512f32f8092c59beba5
Chris Manson <chris@manson.ie>
2021-04-13 09:49:55 +0100

    update with ember-cli-update

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,8 +2,7 @@
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
-
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
     minifyCSS: {
       enabled: false
@@ -14,5 +13,13 @@ module.exports = function(defaults) {
     }
   });
 
-  return app.toTree();
+  /*
+    This build file specifies the options for the dummy test app of this
+    addon, located in `/tests/dummy`
+    This build file does *not* influence how the addon or the app using it
+    behave. You most likely want to be modifying `./index.js` or app's build file
+  */
+
+  const { maybeEmbroider } = require('@embroider/test-setup');
+  return maybeEmbroider(app);
 };
