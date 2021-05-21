c7b38bb984e90700f0d03acb1d4805beebc83325
Florian Pichler <fp@ylk.gd>
2021-05-21 16:34:22 +0200

    Octanify notification container and message

--- addon/components/notification-container.js
+++ addon/components/notification-container.js
@@ -1,26 +1,14 @@
-/* eslint-disable ember/no-classic-components, ember/no-classic-classes, ember/require-tagless-components, prettier/prettier, ember/no-get */
+/* eslint-disable ember/no-computed-properties-in-native-classes, ember/no-classic-components, ember/no-classic-classes, ember/require-tagless-components, prettier/prettier, ember/no-get */
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import { htmlSafe } from '@ember/template';
 import { inject as service } from '@ember/service';
 
-import layout from '../templates/components/notification-container';
+export default class NotificationContainerComponent extends Component {
+  position = 'top';
+  @service notifications;
 
-export default Component.extend({
-  layout,
-  position: 'top',
-  notifications: service(),
-
-  classNameBindings: ['computedPosition', ':ember-cli-notifications-notification__container'],
-  attributeBindings: ['computedStyle:style', 'position:data-test-notification-container'],
-
-  zindex: '1060',
-
-  computedPosition: computed('position', function() {
-    return `ember-cli-notifications-notification__container--${this.get('position')}`;
-  }),
-
-  computedStyle: computed('zindex', function() {
-    return htmlSafe(`z-index: ${this.get('zindex')};`);
-  })
-});
+  @computed('position')
+  get positionClass() {
+    return `ecn_container-${this.position}`;
+  }
+}
--- addon/components/notification-message.js
+++ addon/components/notification-message.js
@@ -1,85 +1,89 @@
-/* eslint-disable ember/no-classic-components, ember/no-classic-classes, prettier/prettier, ember/no-get, ember/no-actions-hash */
+/* eslint-disable ember/no-computed-properties-in-native-classes, ember/no-classic-components, ember/no-classic-classes, ember/require-tagless-components, prettier/prettier, ember/no-get */
 import Component from '@ember/component';
-import Ember from 'ember';
-
 import { htmlSafe } from '@ember/template';
-import { computed } from '@ember/object';
+import { computed, set } from '@ember/object';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
-import layout from '../templates/components/notification-message';
+const availableTypes = ['info', 'success', 'warning', 'error'];
 
-export default Component.extend({
-  layout,
-  tagName: '',
-  notifications: service(),
+export default class NotificationMessage extends Component {
+  tagName = '';
 
-  paused: false,
+  @service notifications;
 
-  dismissClass: computed('notification.dismiss', function() {
-    if (!this.get('notification.dismiss')) return 'c-notification--in';
+  paused = false;
 
-    return false;
-  }),
+  @computed('notification.{htmlContent,message}')
+  get message() {
+    const { htmlContent, message } = this.notification;
+    return htmlContent ? htmlSafe(message) : message;
+  }
 
-  clickableClass: computed('notification.onClick', function() {
-    if (this.get('notification.onClick')) return 'c-notification--clickable';
+  @computed('notification.dismiss')
+  get dismissClass() {
+    return !this.notification.dismiss ? 'ecn_notification-in' : '';
+  }
 
-    return false;
-  }),
+  @computed('notification.onClick')
+  get clickableClass() {
+    return this.notification.onClick ? 'ecn_notification-clickable' : '';
+  }
 
-  notificationSVGPath: computed('notification.type', function() {
-    switch(this.get('notification.type')) {
-      case "error":
-      case "info":
-        return 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z';
-      case "success":
-        return 'M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z';
-      case "warning":
-        return 'M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z';
+  @computed('notification.type')
+  get validType() {
+    let type = this.notification.type;
+
+    if (!type || availableTypes.indexOf(type) === -1) {
+      return '';
     }
 
-    return '';
-  }),
+    return type;
+  }
 
-
-  processedType: computed('notification.type', function() {
-    if (this.get('notification.type') && ['info', 'success', 'warning', 'error'].indexOf(this.get('notification.type')) !== -1 ) {
-      return `c-notification--${this.get('notification.type')}`;
-    }
-
-    return '';
-  }),
+  @computed('validType')
+  get processedType() {
+    return `ecn_notification-${this.validType || 'unknown'}`;
+  }
 
   // Apply the clear animation duration rule inline
-  notificationClearDuration: computed('paused', 'notification.clearDuration', function() {
-    const duration = Ember.Handlebars.Utils.escapeExpression(this.get('notification.clearDuration'));
-    const playState = this.get('paused') ? 'paused' : 'running';
-    return htmlSafe(`animation-duration: ${duration}ms; -webkit-animation-duration: ${duration}ms; animation-play-state: ${playState}; -webkit-animation-play-state: ${playState}`);
-  }),
+  @computed('paused', 'notification.clearDuration')
+  get notificationClearDuration() {
+    let duration = Number(`${this.notification.clearDuration}`.replace(/\D/g, ''));
+    let playState = this.paused ? 'paused' : 'running';
+    let styles = [
+      `-webkit-animation-duration: ${duration}ms`,
+      `animation-duration: ${duration}ms`,
+      `-webkit-animation-play-state: ${playState}`,
+      `animation-play-state: ${playState}`,
+    ].join(';');
 
-  actions: {
-    handleOnClick() {
-      if (this.get('notification.onClick')) {
-        this.get('notification.onClick')(this.get('notification'));
-      }
-    },
-
-    removeNotification() {
-      this.get('notifications').removeNotification(this.get('notification'));
-    },
-
-    handleMouseEnter() {
-      if (this.get('notification.autoClear')) {
-        this.set('paused', true);
-        this.notifications.pauseAutoClear(this.get('notification'));
-      }
-    },
-
-    handleMouseLeave() {
-      if (this.get('notification.autoClear')) {
-        this.set('paused', false);
-        this.notifications.setupAutoClear(this.get('notification'));
-      }
-    },
+    return htmlSafe(styles);
   }
-});
+
+  @action
+  handleOnClick() {
+    this.notification.onClick?.(this.notification);
+  }
+
+  @action
+  removeNotification() {
+    this.notifications.removeNotification(this.notification);
+  }
+
+  @action
+  handleMouseEnter() {
+    if (this.notification.autoClear) {
+      set(this, 'paused', true);
+      this.notifications.pauseAutoClear(this.notification);
+    }
+  }
+
+  @action
+  handleMouseLeave() {
+    if (this.notification.autoClear) {
+      set(this, 'paused', false);
+      this.notifications.setupAutoClear(this.notification);
+    }
+  }
+}
